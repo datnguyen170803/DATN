6e10d905f1dcd6f5f80f475c1a7a5c863de0956b
nguyenhuudat <nguyenhuudat170803@gmail.com>
2024-04-04 09:50:35 +0700

    first commit

--- src/main/java/com/example/config/MailSenderConfig.java
+++ src/main/java/com/example/config/MailSenderConfig.java
@@ -15,8 +15,8 @@ public class MailSenderConfig {
 		JavaMailSenderImpl sender = new JavaMailSenderImpl();
 		sender.setHost("smtp.gmail.com");
 		sender.setPort(587);
-		sender.setUsername("ntvinh234@gmail.com");
-		sender.setPassword("byfzmjlgckozcmtp");
+		sender.setUsername("datnhps23047@fpt.edu.vn");
+		sender.setPassword("pgik uerq gjbp bicz");
 
 		Properties props = sender.getJavaMailProperties();
 		props.put("mail.transport.protocol", "smtp");
